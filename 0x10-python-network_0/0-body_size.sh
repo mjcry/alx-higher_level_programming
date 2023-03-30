fc5090ddfb692c73ed5af9ca2a05f85e680531b0
mjcry <mj.masilo.jack@gmail.com>
2023-03-30 06:46:16 -0700

    aa

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
-# cURL body size
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+#bin/bash
 
+# Store given URL in a variable
+url=$1
+
+# Send request to the given URL and store response body
+response=$(curl -s $url)
+
+# Calculate the size of the response body in bytes
+size=$(echo -n $response | wc -c)
+
+# Print the size of the response body in bytes
+echo $size
