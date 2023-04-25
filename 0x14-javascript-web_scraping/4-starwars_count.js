ead072123ff70646e1ed1931d823403731d8488a
mjcry <mj.masilo.jack@gmail.com>
2023-04-25 12:52:18 -0700

    js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request(apiUrl, function (error, response, body) {
     console.log(error);
   } else {
     const films = JSON.parse(body).results;
-    let count = 0;
+    let count = 1;
     for (const film of films) {
       const characters = film.characters;
       if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
