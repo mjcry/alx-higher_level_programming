327db191b53b5f742a28ef70210b5bcb8b134c0d
mjcry <mj.masilo.jack@gmail.com>
2023-04-25 05:23:30 -0700

    gg

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+
+const request = require('request');
+const url = process.argv[2];
+request
+  .get(url)
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
+  });
